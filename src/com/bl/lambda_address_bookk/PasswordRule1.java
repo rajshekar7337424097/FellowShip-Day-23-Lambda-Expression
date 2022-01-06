3ffab612ec41499b7b34a6c003e39018f6558b56
rajashekar reddy <chrajashekarreddy777@gmail.com>
2022-01-06 09:42:11 +0530

    created password with given condition and it is adding into git hub

---
+++ src/com/bl/lambda_address_bookk/PasswordRule1.java
@@ -0,0 +1,22 @@
+package com.bl.lambda_address_bookk;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+interface IPasswordRule1 {
+	public String validate(String regex, String userEntry );
+}
+public class PasswordRule1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Password as per rule 1");
+		System.out.println("Password contains minimum 8 charectors");
+		String userEntry = sc.next();
+		
+		IPasswordRule1 isPasswordRule1 = (pattern , passwordRule1) -> {
+			return "password give is "+Pattern.compile(pattern).matcher(userEntry).matches();
+		};
+		System.out.println(isPasswordRule1.validate("^[a-z]{8}$", userEntry));
+	}
+
+}
